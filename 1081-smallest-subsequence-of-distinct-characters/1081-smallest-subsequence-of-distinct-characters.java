0d064dfe87e48feed93f7453c02a881c01800666
ANURAG KUSHWAHA <anuragkushwaha2207@outlook.com>
2026-07-19 21:13:58 +0530

    Time: 1 ms (100%), Space: 42.9 MB (65.77%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -1,44 +1,22 @@
 class Solution {
     public String smallestSubsequence(String text) {
-        // Count frequency of each character in the string
-        int[] charFrequency = new int[26];
+        int[] cnt = new int[26];
         for (char c : text.toCharArray()) {
-            charFrequency[c - 'a']++;
+            ++cnt[c - 'a'];
         }
-      
-        // Track which characters are already in the result stack
-        boolean[] isInStack = new boolean[26];
-      
-        // Stack to build the result (using array for efficiency)
-        char[] stack = new char[text.length()];
-        int stackTop = -1;
-      
-        // Process each character in the string
-        for (char currentChar : text.toCharArray()) {
-            // Decrease remaining count for current character
-            charFrequency[currentChar - 'a']--;
-          
-            // Skip if character is already in the result stack
-            if (!isInStack[currentChar - 'a']) {
-                // Remove characters from stack that are:
-                // 1. Greater than current character (not lexicographically smallest)
-                // 2. Will appear again later (have remaining count > 0)
-                while (stackTop >= 0 && 
-                       currentChar < stack[stackTop] && 
-                       charFrequency[stack[stackTop] - 'a'] > 0) {
-                    // Mark the removed character as not in stack
-                    isInStack[stack[stackTop] - 'a'] = false;
-                    stackTop--;
+        boolean[] vis = new boolean[26];
+        char[] cs = new char[text.length()];
+        int top = -1;
+        for (char c : text.toCharArray()) {
+            --cnt[c - 'a'];
+            if (!vis[c - 'a']) {
+                while (top >= 0 && c < cs[top] && cnt[cs[top] - 'a'] > 0) {
+                    vis[cs[top--] - 'a'] = false;
                 }
-              
-                // Add current character to stack
-                stack[++stackTop] = currentChar;
-                // Mark current character as in stack
-                isInStack[currentChar - 'a'] = true;
+                cs[++top] = c;
+                vis[c - 'a'] = true;
             }
         }
-      
-        // Convert stack to string and return
-        return String.valueOf(stack, 0, stackTop + 1);
+        return String.valueOf(cs, 0, top + 1);
     }
 }
